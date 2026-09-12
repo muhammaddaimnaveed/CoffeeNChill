c141592de100a5f880bec5bb5e490c3521ebb221
Muhammad Daim Naveed <st10441685@myemeris.edu.za>
2026-09-12 17:51:47 +0200

    Created GetAllMenuItems to Retrieve All The Menu Items

---
+++ CoffeeNChill/GetAllMenuItems.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using CoffeeNChill.Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace CoffeeNChill.Functions
+{
+    public class GetAllMenuItems
+    {
+        [Function("GetAllMenuItems")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(
+                AuthorizationLevel.Anonymous,
+                "get",
+                Route = "menu")]
+            HttpRequestData req)
+        {
+            try
+            {
+                var service =
+                    new TableStorageService();
+
+                var items =
+                    await service.GetAllMenuItemsAsync();
+
+                var response =
+                    req.CreateResponse(HttpStatusCode.OK);
+
+                await response.WriteAsJsonAsync(items);
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                var response =
+                    req.CreateResponse(
+                        HttpStatusCode.InternalServerError);
+
+                await response.WriteStringAsync(ex.Message);
+
+                return response;
+            }
+        }
+    }
+}
