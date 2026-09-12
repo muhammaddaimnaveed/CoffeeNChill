9a211e7982d4ced9f31f50c1744f3b0438002ee3
Muhammad Daim Naveed <st10441685@myemeris.edu.za>
2026-09-12 17:13:15 +0200

    Created MenuItem Model to Store Menu Item Details

---
+++ CoffeeNChill/Models/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace CoffeeNChill.Models
+{
+    public class MenuItem : ITableEntity
+    {
+        public string PartitionKey { get; set; } = string.Empty;
+
+        public string RowKey { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public double Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public ETag ETag { get; set; }
+
+        public DateTimeOffset? Timestamp { get; set; }
+    }
+}
